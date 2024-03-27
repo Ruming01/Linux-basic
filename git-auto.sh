aa393ffaaf864b6cc8b913879c3f7f67bcbe9633
Ruming01 <202230232@email.daelim.ac.kr>
2024-03-27 12:51:00 +0900

    $CommitName

--- git-auto.sh
+++ git-auto.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-CommitName='none'
+CommitName="none"
 git add .
 echo 'Commit Name : '
 read CommitName
-git commit -m CommitName
+git commit -m $"CommitName"
 git push -u origin main
 exit 0
